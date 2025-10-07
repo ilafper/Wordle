008772604007e77aa590f4bb7b8fc1e9dd0f2bc3
ilafper <ivanaalfaya@gmail.com>
2025-10-07 20:17:33 +0200

    meti patata

--- src/main/java/com/example/wordle/HelloApplication.java
+++ src/main/java/com/example/wordle/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 399);
         stage.setTitle("MI WORDLE!");
-        //aaaaaaaaaa
+        //patata
         stage.setScene(scene);
         stage.show();
 
